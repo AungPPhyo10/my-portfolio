a0a2969de77acc05a005a5722c7f447168180e59
AungPyaePhyo10 <aungpyae.23@ichat.sp.edu.sg>
2025-06-03 10:22:12 +0800

    layout of project cards done

---
+++ app/components/projectCard.js
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+export default function ProjectCard({ id, title, techs, picture }) {
+  const topTechs = techs.slice(0, 3);   // extract the first 3 techs from the array
+
+  return (
+    <div className="card bg-base-200 shadow-md border-1 group border-primary transition duration-200">
+      <figure className="h-48 bg-base-100 p-4 flex justify-center items-center">
+        <img src={picture} alt={title} className="max-h-full object-contain" />
+      </figure>
+
+      <Link href={`/project/${id}`} className="card-body group cursor-pointer flex flex-row items-center justify-between hover:opacity-40">
+        <div>
+          <h2 className="card-title text-base mb-2">{title}</h2>
+          <div className="flex flex-wrap gap-2">
+            {topTechs.map((tech, i) => (
+              <span key={i} className="badge badge-outline">{tech}</span>
+            ))}
+          </div>
+        </div>
+
+        <div className="text-xl transition-transform group-hover:translate-x-1">
+          ▶︎
+        </div>
+      </Link>
+    </div>
+  )
+}
--- app/lib/data.js
+++ app/lib/data.js
@@ -34,7 +34,7 @@ export const education_lists = [
 export const work_lists = [
     {
         id: 1,
-        title: "Singapore Polytechnic - Data Science Analytics Centre",
+        title: "Singapore Poly - Data Science Analytics Centre",
         range: "2025 April - 2026 February",
         description: "Internship",
     },
@@ -72,82 +72,153 @@ export const projects = [
     {
         id: 1, 
         title: "Full-stack Web Application",
+        picture: "logos/nodejs.svg",
         techs: ["Node.js", "Express.js", "JWT Auth", "MySQL", "HTML", "CSS", "Bootstrap"],
         description: "A fantasy space RPG web application where users can build spaceships, do sustainable tasks and compete each other. The project involves creating routes, controllers, and models(MVC structure) to handle database queries integrating with user authentication with JWT token system and game logic.",
-        github: "Github repo link : ",
-        link: "View on live website : "
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        ),
+        link: (
+            <div>
+                View on live server : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 2, 
         title: "Movie app with React.js",
+        picture: "logos/reactjs.svg",
         techs: ["Node.js", "React.js"],
         description: "A web app where you can freely search up movies, actors and set your own favourite movies. Built with React.js v19+. Project data retrieved from themoviedb.org APIs",
-        github: "Github repo link : ",
-        link: "View on live website : "
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        ),
+        link: (
+            <div>
+                View on live server : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 3, 
         title: "Next.js demo application",
+        picture: "logos/nextjs.svg",
         techs: ["Node.js", "React.js", "Next.js"],
         description: "A demo next.js web application that utilizes both Server-Side Rendering(SSR) and Client-Side Rendering(CSR) and other advanced Next.js techniques. The project is a demo tutorial from Udemy course - Next.js and React 18 Bootcamp(by David Joseph Katz)",
-        github: "Github repo link : ",
-        link: "View on live website : "
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        ),
+        link: (
+            <div>
+                View on live server : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 4, 
         title: "E-Library system + CI/CD Pipeline",
-        techs: ["Node.js", "Express.js", "JWT Auth", "Postgresql", "Prisma", "HTML", "CSS", "Bootstrap", "Playwright Testing"],
+        picture: "logos/nodejs.svg",
+        techs: ["Node.js", "Prisma", "Playwright Testing", "Express.js", "JWT Auth", "Postgresql", "HTML", "CSS", "Bootstrap"],
         description: "A library e-book rental system project built by my team for the CICD module requirements. The project involves MVC structure, Prisma as ORM(Object Relational Model) and simulates a library book rental system with user authentication, book renting, returning, admin management, etc",
-        github: "Github repo link : ",
-        link: "View on live website : "
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        ),
+        link: (
+            <div>
+                View on live server : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 5, 
         title: "To-do-list web app with Flask",
+        picture: "logos/flask.svg",
         techs: ["Flask", "HTML", "CSS", "Bootstrap"],
         description: "A simple web app built with python Flask, Jinjja templates for managing tasks and notes",
-        github: "Github repo link : ",
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        ),
     },
     {
         id: 6, 
         title: "Portfolio website",
+        picture: "logos/nextjs.svg",
         techs: ["Node.js", "Next.js", "React.js", "Daisy UI", "Motion-Primitives"],
         description: "My own portfolio website which is being used to host this current webpage",
-        github: "Github repo link : ",
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 7, 
         title: "Gaming Community application prototype",
+        picture: "logos/figma.svg",
         techs: ["Figma"],
         description: "A prototype application for the gaming community built with Figma. The project included conducting research on user needs, solving their needs, documentation, wireframe creation, and prototype development.",
-        link: "View the prototype on Figma : "
+        link: (
+            <div>
+                View prototype on Figma : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 8, 
         title: "Javascript Movie Review Program",
+        picture: "logos/nodejs.svg",
         techs: ["Node.js", "Javascript"],
         description: "A simple command-line driven program where users can play around with existing movies, view details, add new movies, add ratings, etc",
-        github: "Github repo link : ",
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 9, 
         title: "SG University Graduates Employment Survey Program - 1",
+        picture: "logos/nodejs.svg",
         techs: ["Node.js", "Javascript"],
         description: "A command-line driven project that users can interact with to find out data about SG University Graduates employment survey details",
-        github: "Github repo link : ",
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 10, 
         title: "SG University Graduates Employment Survey Program - 2",
+        picture: "logos/nodejs.svg",
         techs: ["Node.js", "Javascript", "HTML", "CSS"],
         description: "A web application that users can interact with to find out about SG University Graduates employment rates, median salaries by majors, etc",
-        github: "Github repo link : ",
+        github: (
+            <div>
+                Github repo link : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     },
     {
         id: 11,
         title: "Low-code Gaming Community application with Mendix",
+        picture: "logos/mendix.svg",
         techs: ["Mendix", "CSS"],
         description: "A prototype version of a gaming community application built with Mendix Studio Pro v9 and node-red. The app included basic page navigation, create/delete operation microflows, in-game chatbot(called via OpenAI endpoint), etc",
-        link: "View demo video : "
+        link: (
+            <div>
+                View on demo video : <a href="https://hello.com" target="_blank"/>
+            </div>
+        )
     }
 ]
